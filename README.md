a692cf72e47e339f2ad4e6ab40744881d6c8fa7e
Lee Ryan 215014186 <ryanlee@deakin.edu.au>
2023-03-26 09:38:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Development Notes
+- functions (subtract, multiply, divide)
+- endpoints via express
+- error handling via try-catch-finally
+- logging via winston library
---
+++ week03-code.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const fs = require('fs');
+const { isNull } = require("util");
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'calculate-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+// error handling
+
+const inputval = (req) => {
+  try {
+    const n1 = parseFloat(req.query.n1);
+    const n2 = parseFloat(req.query.n2);
+    if (isNaN(n1)) {
+      logger.error("n1 is incorrectly defined");
+      throw new Error("n1 incorrectly defined");
+    }
+    if (isNaN(n2)) {
+      logger.error("n2 is incorrectly defined");
+      throw new Error("n2 incorrectly defined");
+    }
+
+    if (n1 === NaN || n2 === NaN) {
+      console.log()
+      throw new Error("Parsing Error");
+    }
+    return [n1, n2];
+  }
+  catch (error) {
+    return [error, null];
+  }
+}
+
+// calculator functions
+const add = (n1, n2) => {
+  return n1 + n2;
+}
+const subtract = (n1, n2) => {
+  return n1 - n2;
+}
+const product = (n1, n2) => {
+  return n1 * n2;
+}
+const divide = (n1, n2) => {
+  return n1 / n2;
+}
+
+app.get("/add", (req, res) => {
+  const [n1, n2] = inputval(req);
+  try {
+    if (n2 === null) {
+      throw n1;
+    } else {
+      logger.info('Parameters ' + n1 + ' and ' + n2 + ' received for addition');
+      const result = add(n1, n2);
+      res.status(200).json({ statuscode: 200, data: result });
+    }
+  }
+  catch (error) {
+    console.error(error)
+    res.status(500).json({ statuscode: 500, msg: error.message })
+  }
+});
+app.get("/subtract", (req, res) => {
+  const [n1, n2] = inputval(req);
+  if (n1 != null) {
+    try {
+      logger.info('Parameters ' + n1 + ' and ' + n2 + ' received for subtraction');
+      const result = subtract(n1, n2);
+      res.status(200).json({ statuscode: 200, data: result });
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ statuscode: 500, msg: error.message })
+    }
+  }
+});
+app.get("/product", (req, res) => {
+  const [n1, n2] = inputval(req);
+  if (n1 != null) {
+    try {
+      logger.info('Parameters ' + n1 + ' and ' + n2 + ' received for product');
+      const result = product(n1, n2);
+      res.status(200).json({ statuscode: 200, data: result });
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ statuscode: 500, msg: error.message })
+    }
+  }
+});
+app.get("/divide", (req, res) => {
+  const [n1, n2] = inputval(req);
+  if (n1 != null) {
+    try {
+      if (n2 == 0) {
+        logger.error("cannot divide by zero");
+        throw new Error("n2 incorrectly defined");
+      }
+      logger.info('Parameters ' + n1 + ' and ' + n2 + ' received for division');
+      const result = divide(n1, n2);
+      res.status(200).json({ statuscode: 200, data: result });
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ statuscode: 500, msg: error.message })
+    }
+  }
+});
+const port = 3040;
+app.listen(port, () => {
+  console.log("hello i'm listening to port " + port);
+})
